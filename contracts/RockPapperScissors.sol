0596f8468cdeace8be5aed7a07724dc857f49b12
Bohdan Skrypka <bskrypka@luxoft.com>
2018-06-07 18:20:09 +0300

    Test added. Added temporary variable.

--- contracts/RockPapperScissors.sol
+++ contracts/RockPapperScissors.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.4;
 
 
 contract RockPaperScissors {
-  enum StatusesData {STARTED,ALL_CHOOSED,ALL_PASS_DECODED,GAME_ENDED};
+  enum StatusesData {STARTED,ALL_CHOOSED,ALL_PASS_DECODED,GAME_ENDED}
 
   struct Game {
     address firstPlayerAddr;
@@ -20,8 +20,8 @@ contract RockPaperScissors {
   }
 
   struct GameMetainfo{
-     bytes32 choicesHashed;
-     uint choices;
+    bytes32 choicesHashed;
+    uint choices;
     uint balances;
   }
 
@@ -47,7 +47,7 @@ contract RockPaperScissors {
   function createGame(address firstPlayer, address secondPlayer)  returns (uint yourGameId){
     require(firstPlayer != 0);
     require(secondPlayer != 0);
-    require(msg.value != 0);
+
 
     Game tempGameData;
     tempGameData.firstPlayerAddr = firstPlayer;
@@ -63,32 +63,36 @@ contract RockPaperScissors {
 
   //2 step - player should make his choice;
   function makeChoice(bytes32 choice, uint gameId) payable returns (bool result){
-    require(gamesMap[gameId].currStatus!=StatusesData.ALL_CHOOSED);
-    require(gamesMap[gameId].currStatus!=StatusesData.GAME_ENDED);
+    Game tempGameData  = gamesMap[gameId];
+
+    require(tempGameData.currStatus!=StatusesData.ALL_CHOOSED);
+    require(tempGameData.currStatus!=StatusesData.GAME_ENDED);
     require(msg.value != 0);
-    require(msg.sender == gamesMap[gameId].firstPlayerAddr || msg.sender == gamesMap[gameId].secondPlayerAddr);
-    gamesMap[gameId].gameInfo[msg.sender].choicesHashed = choice;
-    gamesMap[gameId].gameInfo[msg.sender].balances = msg.value;
-    if (gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].choicesHashed != 0 && gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].choicesHashed != 0) {
-      gamesMap[gameId].currStatus = StatusesData.ALL_CHOOSED;
+    require(msg.sender == tempGameData.firstPlayerAddr || msg.sender == tempGameData.secondPlayerAddr);
+    tempGameData.gameInfo[msg.sender].choicesHashed = choice;
+    tempGameData.gameInfo[msg.sender].balances = msg.value;
+    if (tempGameData.gameInfo[tempGameData.firstPlayerAddr].choicesHashed != 0 && tempGameData.gameInfo[tempGameData.secondPlayerAddr].choicesHashed != 0) {
+      tempGameData.currStatus = StatusesData.ALL_CHOOSED;
     }
+    gamesMap[gameId] = tempGameData;
     emit LogChoice(msg.sender, choice, gameId, msg.value);
     return true;
   }
 
   //3 step - player should check is everybody choose;
-  function isEveryoneChoose(uint gameId) public returns (bool){
-if (gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED){
-    return true;
-}
+  function isEveryoneChoose(uint gameId) public view returns (bool){
+    if (gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED){
+      return true;
+    }
   }
 
   //4 step - Submit and decode password
   function submitPassword(string password, uint gameId) public returns (bool succes){
     require(gameId != 0);
-    require(gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED);
+    Game tempGameData = gamesMap[gameId];
+    require(tempGameData.currStatus == StatusesData.ALL_CHOOSED);
     require(bytes(password).length != 0);
-    require(msg.sender == gamesMap[gameId].firstPlayerAddr || msg.sender == gamesMap[gameId].secondPlayerAddr);
+    require(msg.sender == tempGameData.firstPlayerAddr || msg.sender == tempGameData.secondPlayerAddr);
     checkSelection(password, msg.sender, gameId);
     emit LogPassSubmited(password, msg.sender, gameId);
     return true;
@@ -96,12 +100,13 @@ if (gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED){
 
 
   //5 step - check who wins;
-  function checkWinner(string password, uint gameId) public returns (uint status){
+  function checkWinner(uint gameId) public returns (uint status){
     require(gameId != 0);
-    require(msg.sender == gamesMap[gameId].firstPlayerAddr || msg.sender == gamesMap[gameId].secondPlayerAddr);
-    if (gamesMap[gameId].currStatus == StatusesData.ALL_PASS_DECODED) {
+    Game tempGameData = gamesMap[gameId];
+    require(msg.sender == tempGameData.firstPlayerAddr || msg.sender == tempGameData.secondPlayerAddr);
+    if (tempGameData.currStatus == StatusesData.ALL_PASS_DECODED) {
       if (checkConditions(gameId)) {
-        if (gamesMap[gameId].currStatus != StatusesData.GAME_ENDED) {
+        if (tempGameData.currStatus != StatusesData.GAME_ENDED) {
           setWinnerBenefits(gameId);
           return 1;
         }
@@ -111,11 +116,11 @@ if (gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED){
     }
   }
 
-  function getFirstScore(uint gameId) public returns (uint){
+  function getFirstScore(uint gameId) public view returns (uint){
     return gamesMap[gameId].firstPlayerScore;
   }
 
-  function getSecondScore(uint gameId) public returns (uint){
+  function getSecondScore(uint gameId) public view returns (uint){
     return gamesMap[gameId].secondPlayerScore;
   }
 
@@ -127,62 +132,70 @@ if (gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED){
     require(bytes(userPass).length != 0);
     require(userAddr != 0);
     require(gameId != 0);
-    require(gamesMap[gameId].currStatus == StatusesData.ALL_CHOOSED);
-    require(gamesMap[gameId].gameInfo[userAddr].choicesHashed != 0);
+    Game tempGameData = gamesMap[gameId];
+    require(tempGameData.currStatus == StatusesData.ALL_CHOOSED);
+    require(tempGameData.gameInfo[userAddr].choicesHashed != 0);
 
     for (uint i = 0; i < 3; i++) {
-      if (gamesMap[gameId].gameInfo[userAddr].choicesHashed== keccak256(userPass, i)) {
-        gamesMap[gameId].gameInfo[userAddr].choices= i;
+      if (tempGameData.gameInfo[userAddr].choicesHashed== keccak256(userPass, i)) {
+        tempGameData.gameInfo[userAddr].choices= i;
         emit LogChoicesDecoding(userAddr, i);
       }
     }
-    if (gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].choices != 0 && gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].choices != 0) {
-gamesMap[gameId].currStatus = StatusesData.ALL_PASS_DECODED;
+    if (tempGameData.gameInfo[tempGameData.firstPlayerAddr].choices != 0 && tempGameData.gameInfo[tempGameData.secondPlayerAddr].choices != 0) {
+      tempGameData.currStatus = StatusesData.ALL_PASS_DECODED;
       emit LogAllPassDeconded(true);
     }
+    gamesMap[gameId] = tempGameData;
     return true;
   }
 
 
   //check winned combination
   function checkConditions(uint gameId) private returns (bool success){
-    require(gamesMap[gameId].currStatus == StatusesData.ALL_PASS_DECODED);
-    uint firstChoice = gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].choices;
-    uint secondChoice = gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].choices;
+    require(gameId != 0);
+    Game tempGameData = gamesMap[gameId];
+    require(tempGameData.currStatus == StatusesData.ALL_PASS_DECODED);
+    uint firstChoice = tempGameData.gameInfo[tempGameData.firstPlayerAddr].choices;
+    uint secondChoice = tempGameData.gameInfo[tempGameData.secondPlayerAddr].choices;
     if (firstChoice == secondChoice) {
-      gamesMap[gameId].winningChoince = 4;
+      tempGameData.winningChoince = 4;
       //In case of draw we should stop game
-      gamesMap[gameId].currStatus = StatusesData.GAME_ENDED;
+      tempGameData.currStatus = StatusesData.GAME_ENDED;
     } else if ((firstChoice == 1 || secondChoice == 1) &&
       (firstChoice == 2 || secondChoice == 2)) {
-      gamesMap[gameId].winningChoince = 1;
+      tempGameData.winningChoince = 1;
     } else if ((firstChoice == 2 || secondChoice == 2) &&
       (firstChoice == 3 || secondChoice == 3)) {
-      gamesMap[gameId].winningChoince = 2;
+      tempGameData.winningChoince = 2;
     } else {
-      gamesMap[gameId].winningChoince = 3;
+      tempGameData.winningChoince = 3;
     }
-    emit LogWinnedChoice(gamesMap[gameId].winningChoince);
+    emit LogWinnedChoice(tempGameData.winningChoince);
+    gamesMap[gameId] = tempGameData;
     return true;
   }
 
 
   function setWinnerBenefits(uint gameId) private returns (bool success){
-    require(gamesMap[gameId].winningChoince != 0);
-gamesMap[gameId].currStatus = StatusesData.ALL_PASS_DECODED;
+    require(gameId != 0);
+    Game tempGameData = gamesMap[gameId];
+    require(tempGameData.winningChoince != 0);
+    tempGameData.currStatus = StatusesData.ALL_PASS_DECODED;
 
-    if (gamesMap[gameId].winningChoince == gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].choices) {
-      gamesMap[gameId].firstPlayerScore = gamesMap[gameId].firstPlayerScore + 1;
-      gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].balances += gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].balances;
-      gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].balances= 0;
-      emit LogBenefits(gamesMap[gameId].firstPlayerAddr, gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].balances);
-    } else if (gamesMap[gameId].winningChoince == gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].choices) {
-      gamesMap[gameId].secondPlayerScore = gamesMap[gameId].secondPlayerScore + 1;
-      gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].balances += gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].balances;
-      gamesMap[gameId].gameInfo[gamesMap[gameId].firstPlayerAddr].balances = 0;
-      emit LogBenefits(gamesMap[gameId].secondPlayerAddr, gamesMap[gameId].gameInfo[gamesMap[gameId].secondPlayerAddr].balances);
+    if (tempGameData.winningChoince == tempGameData.gameInfo[tempGameData.firstPlayerAddr].choices) {
+      tempGameData.firstPlayerScore = tempGameData.firstPlayerScore + 1;
+      tempGameData.gameInfo[tempGameData.firstPlayerAddr].balances += tempGameData.gameInfo[tempGameData.secondPlayerAddr].balances;
+      tempGameData.gameInfo[tempGameData.secondPlayerAddr].balances= 0;
+      emit LogBenefits(tempGameData.firstPlayerAddr, tempGameData.gameInfo[tempGameData.firstPlayerAddr].balances);
+    } else if (tempGameData.winningChoince == tempGameData.gameInfo[tempGameData.secondPlayerAddr].choices) {
+      tempGameData.secondPlayerScore = gamesMap[gameId].secondPlayerScore + 1;
+      tempGameData.gameInfo[gamesMap[gameId].secondPlayerAddr].balances += tempGameData.gameInfo[tempGameData.firstPlayerAddr].balances;
+      tempGameData.gameInfo[gamesMap[gameId].firstPlayerAddr].balances = 0;
+      emit LogBenefits(tempGameData.secondPlayerAddr, tempGameData.gameInfo[tempGameData.secondPlayerAddr].balances);
     }
-gamesMap[gameId].currStatus = StatusesData.GAME_ENDED;
+    tempGameData.currStatus = StatusesData.GAME_ENDED;
+    gamesMap[gameId] = tempGameData;
     return true;
   }
 
